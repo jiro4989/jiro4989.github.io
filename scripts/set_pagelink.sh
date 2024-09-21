625b2815226e3baddeffae1410708445a423ca54
jiro4989 <jiroron666@gmail.com>
2024-09-22 03:56:47 +0900

    fix: コマンドの自動インストール処理を削除

--- scripts/set_pagelink.sh
+++ scripts/set_pagelink.sh
@@ -2,12 +2,4 @@
 
 set -eu
 
-if ! command -v generate_markdown_links; then
-  (cd scripts/generate_markdown_links && go install)
-fi
-
-if ! command -v embed_links; then
-  (cd scripts/embed_links && go install)
-fi
-
 find _posts -name '*.md' | generate_markdown_links | embed_links
