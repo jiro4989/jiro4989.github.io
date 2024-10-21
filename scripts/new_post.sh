017eac36a1b61b8d0806999b83e2581ad2c6a4ad
jiro4989 <jiroron666@gmail.com>
2024-10-21 22:03:10 +0900

    fix: 振り返り記事用のパラメータを設定できるようにスクリプトを修正

--- scripts/new_post.sh
+++ scripts/new_post.sh
@@ -6,21 +6,43 @@ err() {
   echo "$(date +"%Y-%m-%d %H:%M:%S") [ERR] $*" >&2
 }
 
-read -r -p "タイトルを入力してください: " title
+title=""
+filename=""
+dt=""
+category=""
+overview="TODO"
 
-read -r -p "ファイル名を入力してください: " filename
+if [[ $# -eq 1 ]] && [[ $1 = furikaeri ]]; then
+  read -r -p "何年の振り返りか入力してください(yyyy): " year
+  title="${year} 年振り返り"
+  filename="furikaeri-${year}"
+  category="daily"
+  overview="${year} 年も終わるので、今年 1 年を振り返る。"
+fi
+
+if [[ "$title" = "" ]]; then
+  read -r -p "タイトルを入力してください: " title
+fi
+
+if [[ "$filename" = "" ]]; then
+  read -r -p "ファイル名を入力してください: " filename
+fi
 if ! [[ "$filename" =~ ^[-a-z0-9]+$ ]]; then
   err "ファイル名は a-z 0-9 - のみ使用可能です: filename = $filename"
   exit 1
 fi
 
-read -r -p "日付を入力してください(yyyy-mm-dd): " dt
+if [[ "$dt" = "" ]]; then
+  read -r -p "日付を入力してください(yyyy-mm-dd): " dt
+fi
 if ! [[ "$dt" =~ ^2[0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$ ]]; then
   err "日付は yyyy-mm-dd です: dt = $dt"
   exit 1
 fi
 
-read -r -p "カテゴリーを入力してください(game, tech, daily, movie): " category
+if [[ "$category" = "" ]]; then
+  read -r -p "カテゴリーを入力してください(game, tech, daily, movie): " category
+fi
 case $category in
   game | tech | daily | movie)
     # 正常系なのでなにもしない
@@ -43,7 +65,7 @@ categories: ${category}
 
 # ${title}
 
-TODO
+${overview}
 
 * Table of contents
 {:toc}
